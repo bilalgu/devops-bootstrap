4a669ee45868f294cd31f44ade8d77e75c938e97
Bilal Guirre <bilal.guirre.pro@proton.me>
2025-05-23 10:55:10 +0200

    connect backend to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,30 @@
 // https://www.npmjs.com/package/express?activeTab=versions
+// https://www.npmjs.com/package/pg?activeTab=versions
 const express = require('express')
 const app = express()
 const port = 8080
+
+const { Client } = require('pg')
+
+const dbClient = new Client({
+    host: process.env.DB_HOST || 'db',
+    user: process.env.DB_USER || 'appuser',
+    password: process.env.DB_PASSWORD || 'mysecretpassword',
+    database: process.env.DB_NAME || 'appdb',
+    port: 5432
+})
+
+dbClient.connect()
+    .then(() => {
+        console.log('Connected to PostgreSQL!')
+        return dbClient.query('SELECT NOW()')
+    })
+    .then(res => {
+        console.log('Time from DB :', res.rows[0])
+    })
+    .catch(err => {
+        console.error('Error connecting to the database :', err)
+    })
 
 app.get('/hello', (req, res) => {
     res.json({ message: 'Hello from Node.js!'})
